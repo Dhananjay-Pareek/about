4c728a75a1a6ff93aa580e8156b63b1e8d53708f
Dhananjay <83391605+Dhananjay-Pareek@users.noreply.github.com>
2021-11-09 09:28:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Web Developer", "Photoshop Editor", "Designer"],
+        strings: ["YouTuber", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Web Developer", "Photoshop Editor", "Designer"],
+        strings: ["YouTuber", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
